8346f561666977ade8b120e4eed7c4f144487a4c
romindo sinaga <32315394+romindosinaga@users.noreply.github.com>
2017-12-12 21:48:45 +0700

    Add files via upload

---
+++ pertemuan ke 9/kereta.cpp
@@ -0,0 +1,146 @@
+#include <iostream>
+using namespace std;
+
+typedef struct { 
+	char nama[30]; //klw pakai tipe data string,panggil nya pakai getline 
+	char ktp[30];
+	string alamat;
+	int umur;
+	char goldarah[2];
+} penumpang;
+
+typedef struct { 
+	string asal;
+	string tujuan;
+	string kereta;
+} kepergian;
+
+typedef struct { 
+	int dd;
+	int hh;
+	int yy;
+} jamkeberangkatan;
+
+typedef struct { 
+	int tgl;
+	string hari;
+	string bulan;
+	int tahun;
+} tanggal;
+
+int main () { 
+
+cout << "===================================================" <<endl;
+cout << "| SELAMAT DATANG DIPROGRAM PESAN TIKET KERETA API |" <<endl;
+cout << "===================================================" <<endl;
+
+penumpang A;// 
+	cout <<"Nama penumpang 	:";gets (A.nama);//getline
+	cout <<"No ktp 		:";cin >> A.ktp;
+	cout <<"alamat		:";cin >> A.alamat;
+	cout <<"Umur 		:";cin >> A.umur;
+	cout <<"Goldarah 	:";cin >> A.goldarah;
+	cout <<endl;
+	
+kepergian B;
+	cout <<"Asal		:";cin >>B.asal;
+	cout <<"Tujuan		:";cin >>B.tujuan;
+	cout <<"Kereta 		:";cin >>B.kereta;
+	cout <<"================================================"<<endl;
+
+jamkeberangkatan C;
+	cout <<"jam 		:";cin >> C.dd; //jam
+	cout <<"menit 		:";cin >> C.hh; //menit
+	cout <<"detik		:";cin >> C.yy; //detik
+
+tanggal D;
+	cout <<"tanggal 	:";cin >> D.tgl;
+	cout <<"hari 		:";cin >> D.hari;
+	cout <<"bulan 		:";cin >> D.bulan;
+	cout <<"tahun 		:";cin >> D.tahun;
+
+cout << "=================================================" <<endl;
+cout << "=================================================" <<endl;
+
+cout <<"tiket kereta api anda adalah :" <<endl;
+cout <<"nama penumpang 	:"<< A.nama<<endl;
+cout <<"No ktp 		:"<< A.ktp<<endl;
+cout <<"alamat		:"<< A.alamat<<endl;
+cout <<"Umur 		:"<< A.umur<<endl;
+cout <<"Goldarah 	:"<< A.goldarah<<endl;
+cout <<"Asal		:"<< B.asal<<endl;
+cout <<"Tujuan		:"<< B.tujuan<<endl;
+cout <<"Kereta 		:"<< B.kereta<<endl;
+cout <<"keberangkatan pada jam : " << C.dd 
+		<<":"<< C.hh
+		<<":"<< C.yy <<endl;
+cout << "berangkat pada tanggal : " << D.tgl 
+		 <<" hari " << D.hari
+		 <<" bulan " << D.bulan
+		 <<" tahun " << D.tahun << endl;
+
+return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+//buat lah sebuah program tiket KA
+//input
+//nama penumpang 
+//no ktp
+//alamat
+//umur 
+//gol darah
+
+//asal 
+//tujuan 
+//jam
+//tanggal
+
+
+//selamat datang do program kareta api 
+//===================================
+//nama penumpang :record 
+//nama penumpang :
+//no ktp
+//alamat
+//umur 
+//gol darah
+
+//kepergian record
+//asal 
+//tujuan 
+//kereta
+
+//jam keberangkatan //record 
+	//jam 
+	//menit 
+	//detik
+//tanggal //recorc
+	//tgl
+	//hari 
+	//bulan 
+	//tahun
+//=======================================
+//cetak 
+	//tiket anda adalah 
+
+//nama penumpang :
+//no ktp
+//alamat
+//umur 
+//gol darah
+
+//asal 
+//tujuan 
+//jam keberangkatan : 17:50:30
+//tanggal
+//================================
